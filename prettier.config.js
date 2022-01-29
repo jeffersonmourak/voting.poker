40568a5fde2d18064f992670f1ade96ea9010997
Jefferson Oliveira <jefferson.oliveira@xa.epicgames.com>
2022-01-28 19:11:18 -0500

    initial package project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    parser: 'typescript',
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+};
